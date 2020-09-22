c3264778a5b3ae45587d5435c95786e8934f039e
mackilroy007 <mackilroy007@gmail.com>
2020-09-22 10:44:28 +0200

    finished exercise 2 and 3

--- Basic/basic1.js
+++ Basic/basic1.js
@@ -1,10 +1,10 @@
-//basic excercise
+//basic excercise 1
 function crystalGazer(jb, loc, parName, nChil) {    //creating function to display Job title, Living location, Partners name and number of children
     
-    document.write("You will be a " + jb + " in " + loc + " and married to " + parName + " with " + nChil + " children.");
+    document.write("You will be a " + jb + " in " + loc + " and married to " + parName + " with " + nChil + " children. <br>");
     //print out the following once defined
 }   
 
 crystalGazer("Programmer", "Vienna", "Anna", "0");  //first input of values into a function crystalGazer
 
-
+//
---
+++ Basic/basic2.js
@@ -0,0 +1,9 @@
+//excercise 2
+
+function ageCalculator(a,b){    //create function that calculates age based on birth year and current year
+    var age = b-a;              //determin the age of the given individual
+    document.write("You will be " + age + " years old this year! <br>");   
+    //return result in a string
+}
+
+ageCalculator(1996,2020);   //input values
