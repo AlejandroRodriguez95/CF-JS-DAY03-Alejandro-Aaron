5e40124605508ed3e88fe72783118d95e2ece1f9
mackilroy007 <mackilroy007@gmail.com>
2020-09-22 10:21:12 +0200

    finished basic 1 excercise

--- Basic/script.js
+++ Basic/script.js
@@ -1,1 +1,10 @@
-console.log("hellow");
+//basic excercise
+function crystalGazer(jb, loc, parName, nChil) {    //creating function to display Job title, Living location, Partners name and number of children
+    
+    document.write("You will be a " + jb + " in " + loc + " and married to " + parName + " with " + nChil + " children.");
+    //print out the following once defined
+}   
+
+crystalGazer("Programmer", "Vienna", "Anna", "0");  //first input of values into a function crystalGazer
+
+
