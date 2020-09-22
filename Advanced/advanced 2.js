2de3a11c53d85fbf0ef88d51d84b7ffed6e51ccd
mackilroy007 <mackilroy007@gmail.com>
2020-09-22 15:16:11 +0200

    changes to the challange and advanced exercise

--- Advanced/advanced 2.js
+++ Advanced/advanced 2.js
@@ -1,11 +1,16 @@
 
 function changeBg() {
-    var a = Math.random() * 255;
-    var b = Math.random() * 255;
-    var c = Math.random() * 255;
+    // var a = Math.random() * 255;
+    // var b = Math.random() * 255;
+    // var c = Math.random() * 255;
 
-    a = Math.round(a);
-    b = Math.round(b);
-    c = Math.round(c);
+    // a = Math.round(a);
+    // b = Math.round(b);
+    // c = Math.round(c);
+
+    var a = Math.floor(Math.random() * 255)
+    var b = Math.floor(Math.random() * 255)
+    var c = Math.floor(Math.random() * 255)
+    
     document.body.style.backgroundColor = "rgb(" + a + "," + b + "," + c + ")";
 }
