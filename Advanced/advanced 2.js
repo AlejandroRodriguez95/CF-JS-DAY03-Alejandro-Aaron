37fb6474a47da7d776beef5ffcc7728ac8b674c0
mackilroy007 <mackilroy007@gmail.com>
2020-09-22 15:18:53 +0200

    final changes to the addvanced exercise

--- Advanced/advanced 2.js
+++ Advanced/advanced 2.js
@@ -8,9 +8,9 @@ function changeBg() {
     // b = Math.round(b);
     // c = Math.round(c);
 
-    var a = Math.floor(Math.random() * 255)
-    var b = Math.floor(Math.random() * 255)
-    var c = Math.floor(Math.random() * 255)
-    
+    var a = Math.floor(Math.random() * 256)
+    var b = Math.floor(Math.random() * 256)
+    var c = Math.floor(Math.random() * 256)
+
     document.body.style.backgroundColor = "rgb(" + a + "," + b + "," + c + ")";
 }
